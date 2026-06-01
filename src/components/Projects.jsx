237bdcc83700e23febf8abb77c61b1a7542abbac
gastontecheradev <acorde.musica10@gmail.com>
2026-06-01 04:38:39 -0300

    Commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,15 @@ const projectUrls = [
   'https://pasearporpasear.somee.com/',
   'https://gestionpro.somee.com/',
   'https://novafadestudio.somee.com/',
+  'https://prepdiplomacia.somee.com/',
 ];
 
 const projectImgs = [
   'https://gastontecheradev.github.io/portfolio/images/proyecto-1.jpg',
   'https://gastontecheradev.github.io/portfolio/images/proyecto-3.jpg',
   'https://gastontecheradev.github.io/portfolio/images/proyecto-2.jpg',
+  'https://gastontecheradev.github.io/portfolio/images/proyecto-4.jpg',
+  
   
 ];
 
